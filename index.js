06ac40dddfd9242a2c2c0332719f3d79cbe2cb67
Harsh Tyagi <117800830+HarshTyagi8010@users.noreply.github.com>
2023-03-19 19:18:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <>
+        <div className="cards">
+            <div className="card">
+                <img src=""atl="myPic" className="card__img"/>
+                <div className="card__info">
+                    <span className="card__category">A NEtflix Original Series</span>
+                    <h3 className="card__title">Dark</h3>
+                    <a href=""target="_blank">
+                        <button>Watch Now</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </>,
+    Document.getElementById("root")
+);
